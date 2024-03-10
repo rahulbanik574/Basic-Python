08c2f7dfce0d3d95f4632adf1e81aa7ffb439a4f
rahulbanik574 <rahulbanik574@gmial.com>
2024-03-10 10:26:56 +0530

    Add initial files

---
+++ global_var.py
@@ -0,0 +1,21 @@
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
+
+print("Variables that are created outside of a function (as in all of the examples above) are known as global variables.
+
+Global variables can be used by everyone, both inside of functions and outside.")
+
+
+x = "awesome"
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
---
+++ var.py
@@ -0,0 +1,12 @@
+x = 6
+y = "rahul"
+print(x)
+print(y)
+
+x = str(3)
+y = int(3)
+z = float(3)
+
+print(x)
+print(y)
+print(z)
